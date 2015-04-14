f957a9cf9f3094d99248ff7902dd27e401d9d120
bracharoths <bracha.b.rothschild@gmail.com>
2015-04-13 21:04:17 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    console.log( 'ready!' );
+ });
+
+ var fizzbuzz = function(number) {
+ 	if (number % 3 === 0 && number% 5 === 0) {
+ 		return 'fizz buzz';
+ 	}
+
+ 	else if (number % 3 === 0) {
+ 			return 'fizz';
+
+ 		} 
+ 	else if (number % 5 === 0) {
+ 			return 'buzz';
+ 		} 
+
+ 	else {
+ 			return number;
+ 		}
+ }
+
+
+ 
+$('form').submit(function(e) {
+ 	var number = $('#enter-number').val();
+ 	$('.output').empty();
+ 	while (number <= 100) {
+ 		$('.output').append("<div>" + fizzbuzz(number) + "</div>");
+ 		number++;
+ 	}
+ 	e.preventDefault();
+ }); 
