25dbf5b3592acee813ff7ac51bdef9aebb952341
bracharoths <bracha.b.rothschild@gmail.com>
2015-07-12 19:18:03 -0600

    added questions as comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-    console.log( 'ready!' );
- });
+    console.log( 'ready!' ); //ensures that whole page is loaded before js runs 
+});
 
- var fizzbuzz = function(number) {
- 	if (number % 3 === 0 && number% 5 === 0) {
+// fizzbuzz: takes a number and returns fizz, buzz, or fizzbuzz (or the number)
+ var fizzbuzz = function(number) { //defines the variable fizzbuzz to funtion 
+ 	if (number % 3 === 0 && number % 5 === 0) { //logic to game. If #/3 and /5 and no remainder, then return fizzbuzz 
  		return 'fizz buzz';
  	}
 
@@ -20,14 +21,15 @@ $(document).ready(function() {
  		}
  }
 
-
- 
-$('form').submit(function(e) {
- 	var number = $('#enter-number').val();
- 	$('.output').empty();
- 	while (number <= 100) {
- 		$('.output').append("<div>" + fizzbuzz(number) + "</div>");
- 		number++;
+//
+ $('form').submit(function(e) {
+ 	var number = $('#enter-number').val(); //assigns number user starts with to variable number and retrieves the value from form. 
+ 	$('.output').empty(); //empties the li's 
+ 	while (number <= 100) { //when the value/# is less than or equal to 100
+ 		$('.output').append("<div>" + fizzbuzz(number) + "</div>"); //numbers should append according to fizzbus funtion. Why need (number)?
+ 		number++; //numbers increase until reaches 100. 
  	}
- 	e.preventDefault();
+ 	e.preventDefault(); //what is the default action here that we're preventing? 
  }); 
+  
+
